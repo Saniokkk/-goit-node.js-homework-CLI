0d53653f90f6dfecdecdb070e5b00d4f9a821906
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-07-10 18:34:02 +0300

    made through command

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const contacts = require("./contacts.js");
-const argv = require('yargs').argv;
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
